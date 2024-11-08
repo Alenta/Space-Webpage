97b8a0122137428cebbb3e6811f7eaf373fa4ce0
Alenta <mef082024@gmail.com>
2024-11-08 10:04:10 +0100

    ok

--- main.js
+++ main.js
@@ -2,9 +2,7 @@ window.navHover = navHover;
 window.navLeave = navLeave;
 function navHover(){
     document.getElementById("links").style.display = "";
-    document.getElementById("logo").style.alignItems = "";
 }
 function navLeave(){
     document.getElementById("links").style.display = "none";
-    document.getElementById("logo").style.alignItems = "center";
 }
